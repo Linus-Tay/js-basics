db548f589134fd94f572a22de6e02a54417f7819
Tiang Ann Linus Tay <s10195119@connect.np.edu.sg>
2021-04-15 08:25:33 +0800

    Enter

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-        // This is my first JavaScript code
-        console.log('Hello World');
+let name = 'Linus';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (e.g. 1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'John'
+let lastName = 'Cena';
+console.log(firstName);
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate)
