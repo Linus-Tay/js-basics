58bb8812d7d35e9a01f855e92bedf9fbcae30eb0
Tiang Ann Linus Tay <s10195119@connect.np.edu.sg>
2021-04-15 11:21:56 +0800

    test2

--- index.js
+++ index.js
@@ -35,56 +35,7 @@ console.log(person.name);
 let selectedColors = ['red', 'blue'];
 selectedColors[2] = 1;
 console.log(selectedColors)
-
-
-
-//** Others **/
-
-let firstName1 = 'Scotty'
-let lastName1 = 'Cena';
-console.log(firstName1);
-
-const interestRate = 0.3;
-console.log(interestRate)
-
-//** Primitive types **/
-
-let name = 'Linus'; // String Literal
-
-// Cannot be a reserved keyword
-// Should be meaningful
-// Cannot start with a number (e.g. 1name)
-// Cannot contain a space or hyphen (-)
-// Are case-sensitive
-
-let age = 30; // Number Literal
-let isApproved = true; // Boolean Literal
-let firstName = undefined;
-let selectedColor = null;
-console.log(name);
-
-
-//** Objects **/
-
-let person = {
-    name: 'Linus',
-    age: 30
-};
-
-// Dot Notation
-person.name = 'John';
-
-//Bracket Notation
-person['name'] = 'Mary';
-
-console.log(person.name);
-
-//** Arrays **/
-
-let selectedColors = ['red', 'blue'];
-selectedColors[2] = 1;
-console.log(selectedColors)
-
+console.log(selectedColors.length)
 
 
 //** Others **/
@@ -96,5 +47,20 @@ console.log(firstName1);
 const interestRate = 0.3;
 console.log(interestRate)
 
-// Please work for the love of God
-// How about now
+
+//** Functions **//
+
+// Perfoming a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Calculating a value
+function square(number) {
+    return number * number;
+}
+
+greet(name, lastName1);
+
+let number = square(2);
+console.log(number);
